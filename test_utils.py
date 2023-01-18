5d4002d58d82a7f10a1012f49448293d138cb159
saberdevlpmt <saberachiba@gmail.com>
2023-01-18 16:29:45 +0100

    test fonction les_potions

--- test_utils.py
+++ test_utils.py
@@ -7,8 +7,11 @@ class TestJeux(unittest.TestCase):
         pass
 
     def test_les_potions(self):
-        pass
-    
+        self.assertGreater(les_potions(45,50,3) ,(45,50,2))
+        self.assertGreater(les_potions(12,50,2) ,(12,50,1))
+        self.assertGreater(les_potions(37,50,1) ,(37,50,0))
+        
+        
     def test_choix_du_joueur(self):
         pass
     
