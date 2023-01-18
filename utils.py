609e924125fb5259f6831bb59e01fa0e3d8615dc
Dylan Vanstavel <vanstavel.dylan@outlook.fr>
2023-01-18 11:57:49 +0100

    Fonction choix_du_joueur

--- utils.py
+++ utils.py
@@ -8,10 +8,16 @@ def les_potions():
     pass
 
 def choix_du_joueur():
-    pass
-
+    choix = int(input("Entrez 1 pour attaquer ou 2 pour prendre une potion de vie : "))
+    tour_fini= False
+    while not tour_fini:
+        if choix == 1:
+            tour_fini = True
+            attaquer()
+        elif choix == 2:
+            tour_fini = True
+            les_potions()
+        else:
+            choix = int(input("Erreur, entrez à nouveau votre choix, 1 pour attaquer ou 2 pour prendre la potion de soin :"))
 def verification_vitoire_defaite(vie, partie_en_cours):
-    pass
-
-def attaque_bot():
     pass
