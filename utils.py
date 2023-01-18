ca5015c6dcdfb6b0cdaec7d7ca9833b18e856a26
Thomas59 <thomas.lemay59@outlook.fr>
2023-01-18 11:33:50 +0100

    fonction attaquer et verification ajouter

--- utils.py
+++ utils.py
@@ -1,8 +1,21 @@
+import random
+
+vie_joueur = 50
+
 def affichage():
     pass
 
 def attaquer(vie):
-    pass
+    degat = random.randint(0, 10)
+    if degat > 7:
+        print("coup critique")
+    elif degat < 7 and degat > 0:
+        print("coup normal")
+    elif degat == 0:
+        print("Esquive")
+    vie = vie - degat 
+    return vie
+    
 
 def les_potions():
     pass
@@ -11,7 +24,8 @@ def choix_du_joueur():
     pass
 
 def verification_vitoire_defaite(vie, partie_en_cours):
-    pass
-
-def attaque_bot():
-    pass
+     if vie_joueur <= 0:
+         print("Le Monstre a gg")
+     elif vie_monstre <= 0:
+         print("Le Joueur a gg")
+     partie_en_cours = False
