3e98b8f11fc795ce957bc94196127ec768cc6ec4
saberdevlpmt <saberachiba@gmail.com>
2023-01-19 11:22:58 +0100

    teste de la fonction les_potions

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 from utils import affichage , les_potions , choix_du_joueur , attaquer , verification_vitoire_defaite
 
 class TestJeux(unittest.TestCase):
@@ -6,10 +7,22 @@ class TestJeux(unittest.TestCase):
     def test_attaquer(self):
         pass
 
+    # def test_les_potions(self):
+    #     random.seed(45)
+    #     self.assertEqual(les_potions(45,50,3) ,(45,50,2))
+    #     self.assertEqual(les_potions(12,50,2) ,(12,50,1))        
+    #     self.assertEqual(les_potions(37,50,1) ,(37,50,0))
+        
+        
     def test_les_potions(self):
-        self.assertGreater(les_potions(45,50,3) ,(45,50,2))
-        self.assertGreater(les_potions(12,50,2) ,(12,50,1))
-        self.assertGreater(les_potions(37,50,1) ,(37,50,0))
+        self.assertGreaterEqual(les_potions(45,50,3)[0],45)
+        self.assertEqual(les_potions(12,50,2)[1],50)
+        self.assertLessEqual(les_potions(37,50,1)[2],0)
+        
+        
+    
+    
+   
         
         
     def test_choix_du_joueur(self):
