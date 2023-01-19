1feccd1dc55956bf2bc37571087a3eadd1bdccee
Dylan Vanstavel <vanstavel.dylan@outlook.fr>
2023-01-19 11:30:11 +0100

    test choix du joueur

--- test_utils.py
+++ test_utils.py
@@ -11,6 +11,16 @@ class TestJeux(unittest.TestCase):
     
     def test_choix_du_joueur(self):
         pass
+        # vie_joueur = 50
+        # vie_monstre = 50
+        # nombre_potions = 3
+        # choix = 1
+        # retour = choix_du_joueur(vie_joueur, vie_monstre, nombre_potions)
+        # self.assertGreater(retour[0], vie_joueur)
+        # self.assertLess(retour[1], vie_monstre)
+        # self.assertEqual(retour[2], nombre_potions)
     
     def test_verification_victoire_defaite(self):
         pass
+    
+    
