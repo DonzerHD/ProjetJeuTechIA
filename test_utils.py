f18b99df991e601a1a8f1cd137b5a320e1b7965b
DonzerHD <thomas.lemay59@outlook.fr>
2023-01-19 10:46:20 +0100

    fonction test_attaquer finit

--- test_utils.py
+++ test_utils.py
@@ -4,11 +4,35 @@ from utils import affichage , les_potions , choix_du_joueur , attaquer , verific
 class TestJeux(unittest.TestCase):
 
     def test_attaquer(self):
-        vie_joueur = 100
-        vie_monstre = 50
-
+        vie_joueur = 50
+        vie_monstre = 100
+        
         vie_joueur, vie_monstre = attaquer(vie_joueur, vie_monstre)
-        self.assertLess(vie_monstre, 50)
+        
+        self.assertTrue(vie_monstre <= 100)  
+        self.assertFalse(vie_monstre <= 0)
+        self.assertEqual(vie_joueur, 50)
+        
+        vie_joueur = 50
+        vie_monstre = 100
+        
+        vie_monstre, vie_joueur= attaquer(vie_monstre, vie_joueur)
+        
+        self.assertTrue(vie_joueur <= 50)
+        self.assertFalse(vie_joueur <= 0)
+        self.assertEqual(vie_monstre, 100)
+        
+        vie_joueur = 50
+        vie_monstre = 100
+        for i in range(100):
+            vie_joueur, vie_monstre = attaquer(vie_joueur, vie_monstre)
+            self.assertTrue(vie_monstre <= 100)
+        
+        vie_joueur = 50
+        vie_monstre = 100
+        for i in range(100):
+            vie_monstre, vie_joueur = attaquer(vie_monstre, vie_joueur)
+            self.assertTrue(vie_joueur <= 50)
 
     def test_les_potions(self):
         pass
